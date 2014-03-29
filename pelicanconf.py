89702a9c5e7ddbf95de1e476df6530c68fe9da65
Tim White <tim@whiteitsolutions.com.au>
2014-03-29 20:51:59 +1000

    fix category and tag pages

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,10 +20,10 @@ PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
 
 CATEGORY_URL = 'category/{slug}'
-CATEGORY_SAVE_AS = 'category/{slug}.html'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
 
 TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}.html'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
 
 
 
